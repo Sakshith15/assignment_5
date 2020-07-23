0206aeb80141460d1d20010c2f215c74b0897e2a
Sakshith15 <60150108+Sakshith15@users.noreply.github.com>
2020-07-23 23:40:37 +0530

    Add files via upload

---
+++ src/main/java/com/epam/Sakshith_Logging/Sakshith_Epam.java
@@ -0,0 +1,14 @@
+package com.epam.Sakshith_Logging;
+
+import java.io.*;
+import java.io.IOException;
+
+public class Sakshith_Epam {
+	public static void main(String args[]) throws IOException
+	{
+		Simple_and_Compound_Interest obj = new Simple_and_Compound_Interest();
+		obj.calculateInterest();
+		House_Construction_Cost_Estimation obj1 = new House_Construction_Cost_Estimation();
+		obj1.costCalculation();
+	}
+}
